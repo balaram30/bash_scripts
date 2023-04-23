fddbbd43c57123825efda4e7b55ef12bd3c861b2
balaram30 <balarammohanty30@gmail.com>
2023-04-23 20:45:06 +0530

    Updated to save the files to the required directories
    The update include 2 arrays one which contains the file path of all the testcases and another contains the path to the directories on which the files need to be pushed
    This filename was compared with the basename of the filepath and accordingly the desired array of directories was used to push the file
    
    Issue : extraction of only the required path directories are saved manually onto an array this will result into a lot of manual editing afterwards to avioid this a method need to be found out to do this effectively

--- clearDir/clearDir.sh
+++ clearDir/clearDir.sh
@@ -18,11 +18,77 @@ then
 	#find /home/$USER/$Dir1 -type f -delete
 	sudo find $Dir1 -type f -delete
 
-elif [ $usrResp = 'n' ] || [ $usrResp = 'N' ] 
-then
-	echo "files not deleted"
-	
-else
-	echo "Invalid input"
-
 fi
+
+#tree -fJ /home/balaram/pointNode
+
+dir_array_point=(/home/balaram/pointNode/fourRelay/multiplierSlave /home/balaram/pointNode/fourRelay/Node /home/balaram/pointNode/fourRelay/Touch/BS814C /home/balaram/pointNode/fourRelay/Touch/BS814C_Neo /home/balaram/pointNode/fourRelay/Touch/BS814C_Neo_Int /home/balaram/pointNode/HighCapacity/oneRelay/Touch/BS814C_Neo /home/balaram/pointNode/HighCapacity/twoRelay/Touch/BS814C_Neo /home/balaram/pointNode/masterTWW/pointNode/fourTouch/BS814C /home/balaram/pointNode/masterTWW/pointNode/fourTouch/BS814C_Neo /home/balaram/pointNode/masterTWW/pointNode/oneTouch/BS814C /home/balaram/pointNode/masterTWW/pointNode/oneTouch/BS814C /home/balaram/pointNode/masterTWW/pointNode/threeTouch/BS814C /home/balaram/pointNode/masterTWW/pointNode/threeTouch/BS814C_Neo /home/balaram/pointNode/masterTWW/pointNode/twoTouch/BS814C /home/balaram/pointNode/masterTWW/pointNode/twoTouch/BS814C_Neo /home/balaram/pointNode/oneRelayFB/Touch/BS814C_Neo /home/balaram/pointNode/oneRelay/multiplierSlave /home/balaram/pointNode/oneRelay/Node /home/balaram/pointNode/oneRelay/Touch/BS814C /home/balaram/pointNode/oneRelay/Touch/BS814C_Neo /home/balaram/pointNode/oneRelay/Touch/BS814C_Neo_Int /home/balaram/pointNode/twoRelay/multiplierSlave /home/balaram/pointNode/twoRelay/Node /home/balaram/pointNode/twoRelay/Touch/BS814C /home/balaram/pointNode/twoRelay/Touch/BS814C_Neo /home/balaram/pointNode/twoRelay/Touch/BS814C_Neo_Int /home/balaram/pointNode/threeRelay/multiplierSlave /home/balaram/pointNode/threeRelay/Node /home/balaram/pointNode/threeRelay/Touch/BS814C /home/balaram/pointNode/threeRelay/Touch/BS814C_Neo /home/balaram/pointNode/threeRelay/Touch/BS814C_Neo_Int)
+
+dir_array_bldc=(/home/balaram/bldcDimmer/Node /home/balaram/bldcDimmer/Touch/BS814C_Neo)
+
+testcases_dir_path="/home/balaram/Testcases_8266"
+# files_to_move_arr is the array containing the directories of all the files contained in the testcases path
+mapfile -t files_to_move_arr < <(find "$testcases_dir_path" -type f)
+
+for file_dir in "${files_to_move_arr[@]}"; do
+
+	if [ "$(basename "$file_dir")" = "PointNode_testcases.ods" ]
+	then
+
+		for folder in "${dir_array_point[@]}"; do
+	        	cp "$file_dir" "$folder"
+    
+		done 
+		
+	elif [ "$(basename "$file_dir")" = "CapacitiveFanDimmer_testcases.ods" ]
+	then 
+	
+		for folder in "${dir_array_bldc[@]}"; do
+	        	cp "$file_dir" "$folder"
+    
+		done 
+	
+	fi
+done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
